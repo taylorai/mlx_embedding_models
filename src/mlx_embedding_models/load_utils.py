fd306f90eed1c1ccf13b99e434a42b1630852506
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-03-07 15:40:30 -0800

    fix convert

--- src/mlx_embedding_models/load_utils.py
+++ src/mlx_embedding_models/load_utils.py
@@ -39,8 +39,11 @@ def replace_key_distilbert(key: str) -> str:
     key = key.replace("vocab_projector", "lm_head.decoder")
 
 
-def convert(bert_model: str) -> None:
-    model = BertModel.from_pretrained(bert_model)
+def convert(bert_model: str, lm_head: bool = False):
+    if not lm_head:
+        model = BertModel.from_pretrained(bert_model)
+    else:
+        model = BertForMaskedLM.from_pretrained(bert_model)
     # save the tensors
     tensors = {
         replace_key(key): tensor.numpy() for key, tensor in model.state_dict().items()
@@ -48,8 +51,11 @@ def convert(bert_model: str) -> None:
     # print([n for n, p in tensors.items()])
     return tensors
 
-def convert_distilbert(distilbert_model: str) -> None:
-    model = DistilBertModel.from_pretrained(distilbert_model)
+def convert_distilbert(distilbert_model: str, lm_head: bool = False):
+    if not lm_head:
+        model = DistilBertModel.from_pretrained(distilbert_model)
+    else:
+        model = DistilBertForMaskedLM.from_pretrained(distilbert_model)
     # save the tensors
     tensors = {
         replace_key_distilbert(key): tensor.numpy() for key, tensor in model.state_dict().items()
